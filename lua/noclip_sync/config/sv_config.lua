4e6f6973456f1c179319ad0ce5d43f5f2854cae0
Owain <owain.jones74@gmail.com>
2022-01-29 11:54:22 +0000

    basis

---
+++ lua/noclip_sync/config/sv_config.lua
@@ -0,0 +1,8 @@
+-- Your community's API key. Can be found here: https://mycommunity.noclip.me/dashboard/forums/settings/misc
+NoClip.Sync.Config.APIKey = "123-ABC-123-ABC"
+
+-- The URL to your community. Ensure there is no trailing "/"
+NoClip.Sync.Config.URL = "https://mycommunity.noclip.me"
+
+-- This server's ID. It can be found by selecting the server here: https://mycommunity.noclip.me/dashboard/servers
+NoClip.Sync.Config.ServerID = "123ABC"
